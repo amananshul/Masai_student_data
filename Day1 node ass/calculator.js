8d743ec80b1dc2661e5168463122c2c3b3855c72
aman <aman004dubey@gmail.com>
2022-06-24 00:03:17 +0530

    ok

--- Day1 node ass/calculator.js
+++ Day1 node ass/calculator.js
@@ -1,35 +1,35 @@
 // console.log(process.argv[2])
-let arg = process.argv[2];
+let argument= process.argv[2];
 const crypto = require("crypto");
 
-if(arg==="add"){
+if(arg==="addition"){
     const sum = Number(process.argv[3])+Number(process.argv[4])
     console.log(sum);
 }
-else if(arg === "sub"){
+else if(argument=== "subtract"){
     const diff = Number(process.argv[3])-Number(process.argv[4]);
     console.log(diff);
 }
-else if(arg === "mult"){
+else if(argument=== "multiply"){
     const pro = Number(process.argv[3])*Number(process.argv[4]);
     console.log(pro);
 }
-else if(arg === "divide"){
+else if(argument=== "divide"){
     const ans = Number(process.argv[3])/Number(process.argv[4]);
     console.log(ans);
 }
-else if(arg === "sin"){
+else if(argument=== "sin"){
     const ans = Math.sin(Number(process.argv[3]));
     console.log(ans);
 }
-else if(arg === "cos"){
+else if(argument=== "cos"){
     const ans = Math.cos(Number(process.argv[3]));
     console.log(ans);
 }
-else if(arg === "tan"){
+else if(argument=== "tan"){
     const ans = Math.tan(Number(process.argv[3]));
     console.log(ans);
 }
-else if(arg === "random"){
+else if(argument=== "random"){
     console.log(crypto.randomInt(0,100));
 }
